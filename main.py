1a5ff539cb7d1ee12a0b0a3fa2865db67d8a1e03
jose <jdiazseg9611@universidadean.edu.co>
2019-09-12 13:51:44 -0500

    Ejercicio finalizado y comprobado

--- main.py
+++ main.py
@@ -1,38 +1,35 @@
 import math
+from fractions import Fraction
+
 def resta(n1, d1, n2, d2):
     n3 = n1 * d2 - d1 * n2
     d3 = d1 * d2
-    mcd = math.gcd(n3, d3)
-    n3 //= mcd
-    d3 //= mcd
-
-    return n3, d3
-
+    return Fraction(n3, d3)
 
 def restaMal(n1, d1, n2, d2):
     n3 = n1 - n2
     d3 = d1 - d2
-    mcd = math.gcd(n3, d3)
-    n3 //= mcd
-    d3 //= mcd
-    if (n1 > d1 or (mcd == 1 and n3 == -1 and d3 < 0)):
+    if (n1 > d1 or (n3 == -1 and d3 < 0)):
         n3 = int(math.fabs(n3))
         d3 = int(math.fabs(d3))
-    return n3, d3
-
+    return Fraction(n3, d3)
 
 linea = input()
 while linea != "0 0":
     datos = linea.strip().split(' ')
     b = int(datos[0])
     n = int(datos[1])
-    x = ""
+    listax = []
     for a in range(10):
         for m in range(1,30):
-            if(a != b and b != m):
+            if(a != b and n != m):
                 val1 = resta(a, m, b, n)
                 val2 = restaMal(a, m, b, n)
                 if (val1 == val2):
-                    x += str(a)+"/"+str(m) + " "
-    print(x)
+                    listax.append((a,m))
+    listax.sort(key=lambda tup: tup[1],reverse = True)
+    x = ""
+    for (i,j) in listax:
+        x += str(i)+ "/" + str(j)+" "
+    print(x[:-1])
     linea = input()
